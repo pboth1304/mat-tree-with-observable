9d6d75b9ddbb621d22acc079607ba34470bab56a
pascalbothner <pascal.bothner@outlook.de>
2020-01-20 20:39:06 +0100

    added component with dynamic folder tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,32 @@
-import { Component } from '@angular/core';
+import { CollectionViewer, SelectionChange } from '@angular/cdk/collections';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { Component, Injectable } from '@angular/core';
+import { BehaviorSubject, merge, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { MatTreeFlattener, MatTreeFlatDataSource } from '@angular/material';
+
+interface Folder {
+  id: string;
+  title: string;
+  children?: Folder[];
+}
+
+const FOLDERS: Folder[] = [
+  {
+    id: '1',
+    title: 'Root',
+    children: [
+      { id: '2', title: 'subfolder 1' },
+      { id: '3', title: 'subfolder 2' },
+      { id: '4', title: 'subfolder 3' },
+      {
+        id: '5',
+        title: 'subfolder 4',
+        children: [{ id: '51', title: 'sub subfolder 1' }]
+      }
+    ]
+  }
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +34,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mat-tree-with-ngrx';
+  levels = new Map<Folder, number>();
+
+  treeFlattener: MatTreeFlattener<Folder, Folder>;
+
+  treeControl: FlatTreeControl<Folder>;
+
+  dataSource: MatTreeFlatDataSource<Folder, Folder>;
+
+  constructor() {
+    // constructor(private readonly store:Store<YourState>) {}
+
+    this.treeFlattener = new MatTreeFlattener(
+      this.transformer,
+      this.getLevel,
+      this.isExpandable,
+      this.getChildren
+    );
+
+    this.treeControl = new FlatTreeControl<Folder>(
+      this.getLevel,
+      this.isExpandable
+    );
+
+    this.dataSource = new MatTreeFlatDataSource(
+      this.treeControl,
+      this.treeFlattener
+    );
+
+    // this.store.pipe(select(getFolders)).subscribe(folders => (this.dataSource.data = folders));
+    of(FOLDERS).subscribe(folders => (this.dataSource.data = folders));
+
+    setTimeout(() => {
+      this.dataSource.data = [
+        ...FOLDERS,
+        {
+          id: '15215',
+          title: 'subfoldeager 1',
+          children: [{ id: '21', title: 'subfolder 1212' }]
+        }
+      ];
+    }, 3000);
+  }
+
+  getLevel = (node: Folder): number => {
+    return this.levels.get(node) || 0;
+  };
+
+  transformer = (node: Folder, level: number) => {
+    this.levels.set(node, level);
+    return node;
+  };
+
+  isExpandable = (node: Folder) => {
+    return node.children ? true : false;
+  };
+
+  hasChildren = (_: number, nodeData: Folder) => {
+    return nodeData.children && nodeData.children.length;
+  };
+
+  getChildren = (node: Folder) => {
+    return node.children;
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,105 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { A11yModule } from '@angular/cdk/a11y';
+import { BidiModule } from '@angular/cdk/bidi';
+import { ObserversModule } from '@angular/cdk/observers';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { PlatformModule } from '@angular/cdk/platform';
+import { PortalModule } from '@angular/cdk/portal';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule
+} from '@angular/material';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/**
+ * NgModule that includes all Material modules that are required to serve
+ * the Plunker.
+ */
+@NgModule({
+  exports: [
+    // CDK
+    A11yModule,
+    BidiModule,
+    ObserversModule,
+    OverlayModule,
+    PlatformModule,
+    PortalModule,
+    ScrollingModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+
+    // Material
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatNativeDateModule,
+    MatTreeModule
+  ]
+})
+export class MaterialModule {}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
